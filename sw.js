b52f17efe499087a82a56d0b341aa47c9ad05a34
Austin Bartholomaus <abartholomaus5@gmail.com>
2026-09-09 18:24:39 -0500

    sw

--- sw.js
+++ sw.js
@@ -31,7 +31,7 @@
 // gated-activation pattern below (waiting worker + SKIP_WAITING message on
 // user consent) is adopted for predictability of *when* control transfers,
 // not because that was an active bug for this app's architecture.
-const SW_BUILD = '20260909T2321Z-3657198';
+const SW_BUILD = '20260909T2324Z-034330f';
 
 const CACHE_NAME = `plant-asset-${SW_BUILD}`;
 
